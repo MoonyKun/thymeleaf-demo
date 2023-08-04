fcea85bb7ae7889519af71c27306edf697c904db
Moonykun <fullmoonwithyou@163.com>
2023-08-04 15:35:53 +0800

    Landing page implementation supplement

--- src/main/java/com/moonykun/demo/controller/CommonController.java
+++ src/main/java/com/moonykun/demo/controller/CommonController.java
@@ -12,4 +12,9 @@ public class CommonController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("/index")
+   public String index() {
+        return "index";
+   }
 }
